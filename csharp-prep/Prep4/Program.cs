25e2c257bc5bad6a8e0e2d22f65d7f33d4968c9c
iCarrin isaiahcarrin@gmail.com <isaiahcarring@gmail.com>
2023-11-13 19:16:10 -0700

    Made Loops prep assignment

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,50 @@
 using System;
+using System.Net;
+//using System.Collections.Generic;
+
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> allNumbers = new List<int>();
+        int sumAllNumbers = 0;
+        int aveAllNumbers = 0;
+        int largestNumber = 0;
+
+        Console.Write("Give me a number (type \"0\" to stop)");
+        string response = Console.ReadLine();
+        int number = int.Parse(response);
+        allNumbers.Add (number);
+        
+
+        while (number != 0)
+        {
+            response = Console.ReadLine();
+            number = int.Parse(response);
+            allNumbers.Add (number);
+        }
+
+        foreach (int num in allNumbers)
+        {
+            sumAllNumbers += num;
+        }
+
+        aveAllNumbers = sumAllNumbers / allNumbers.Count;
+        
+        foreach (int num in allNumbers)
+        {
+            if (largestNumber < num)
+            {
+                largestNumber = num;
+            }
+            
+        }
+
+        Console.WriteLine($"The sum in {sumAllNumbers}");
+        Console.WriteLine($"The average is {aveAllNumbers}");
+        Console.WriteLine($"The largest is {largestNumber}");
+
+
     }
 }
