6f090d63ebefa19ef5532b2661864a710f8f14a3
iCarrin isaiahcarrin@gmail.com <isaiahcarring@gmail.com>
2023-11-13 20:39:19 -0700

    Made the functions prep

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string name = PromptUserName();
+        int num = PromptUserNumber();
+        int sqNum = SquareNumber(num);
+        DisplayResults(name, sqNum);
+
+    }
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    static String PromptUserName()
+    {
+        Console.WriteLine("What is your name?");
+        string userName = Console.ReadLine();
+        return userName;
+    }
+
+
+    static int PromptUserNumber()
+    {
+       Console.WriteLine("What is your favorite number?");
+       string favStringNum = Console.ReadLine();
+       int favNumber = int.Parse(favStringNum);
+       return favNumber;
+    }
+
+    static int SquareNumber(int favoriteNumber)
+    {
+        int favNumSq = favoriteNumber * favoriteNumber;
+        return favNumSq;
+    }
+    static void DisplayResults(string name, int favNumSq)
+    {
+        Console.WriteLine($"{name} your favorite number squared is {favNumSq}");
     }
 }
