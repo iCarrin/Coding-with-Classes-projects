7e26310f38eaa40f93d7015b76987ef6db9f7102
iCarrin isaiahcarrin@gmail.com <isaiahcarring@gmail.com>
2023-11-21 21:14:55 -0700

    test

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,15 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
+        string[] split;
         string words = "Hi, my name is carl";
 
-        string[] split = words.Split();
+        split = words.Split();
 
-        Console.WriteLine(words);
+        Console.WriteLine(split);
+        
 
-        foreach (string word in split)
+        // Console.WriteLine(split[0]);
+        // Console.WriteLine(split[1]);
+        // Console.WriteLine(split[2]);
+        // Console.WriteLine(split[3]);
+        // Console.WriteLine(split[4]);
+        
+        for (int i = 0; i < split.Length; i++)
         {
-            Console.Write(word);
+          
+            
+            if (i < 3) 
+            {
+                Console.Write(split[i]);
+            }
         }
     }
 }
